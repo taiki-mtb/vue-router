63679cb5eb4a4206ae669902fb0e0d04c3083b7f
Matoba Taiki <tomatomatobanana@gmail.com>
2020-11-02 11:57:50 +0900

    vue routerについて勉強

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div style="width: 700px; margin: auto; padding-top: 50px;">
-    <nav>
-      <router-link
-        to="/"
-        active-class="link--active"
-        exact
-        class="link"
-        >Home</router-link>
-      <router-link
-        to ="/users"
-        active-class="link--active"
-        exact
-        class="link"
-        >Users</router-link>
-    </nav>
-    <router-view></router-view>
+    <router-view name="header"></router-view>
+    <transition
+      name="fade"
+      mode="out-in"
+      @before-enter="beforeEnter"
+    >
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
+<script>
+  export default {
+    methods: {
+      beforeEnter() {
+        this.$root.$emit('triggerScroll');
+      }
+    }
+  };
+</script>
+
 <style scoped>
-  .link {
-    margin-right: 10px;
-  }
-  .link--active {
-    font-size: 20px;
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+    }
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s;
   }
 </style>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -7,6 +7,14 @@
     <button @click="toHome">Homeに行く</button>
     <hr>
     <h1>User No. {{ id }}</h1>
+    <router-link :to="'/users/' + (Number(id) + 1) + '/profile' ">次のユーザー</router-link>
+    <router-view></router-view>
+    <div style="height: 700px;"></div>
+    <router-link
+      id="next-user"
+      :to="{ name: 'users-id-profile', params: { id: Number(id) + 1 }, query: { lang: 'ja', page: 2 }, hash: '#next-user' }"
+      >次のユーザー</router-link>
+    <div style="height: 1400px;"></div>
   </div>
 </template>
 
@@ -16,6 +24,22 @@
     methods: {
       toHome() {
         this.$router.push("/");
+      }
+    },
+    beforeRouteEnter(to, from, next) {
+      next(vm => {
+        console.log(vm.id);
+      });
+    },
+    beforeRouteUpdate(to, from, next) {
+      next();
+    },
+    beforeRouteLeave(to, from, next) {
+      const isLeave = window.confirm("離れますか");
+      if (isLeave) {
+        next();
+      } else {
+        next(false);
       }
     }
   };
